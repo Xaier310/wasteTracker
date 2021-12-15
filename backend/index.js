83157304692c9ca090089cce11bb5735a2c211ca
Amit Batra <75168062+amitbatra31@users.noreply.github.com>
2021-12-15 16:52:55 +0530

    Heroku deploy

--- backend/index.js
+++ backend/index.js
@@ -10,13 +10,12 @@ const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const path = require("path");
-var cors = require('cors');
+var cors = require("cors");
 var ajax = require("ajax");
 
-app.use(cors())
+app.use(cors());
 
 app.use(express.json());
-
 
 //setup passport
 app.use(
@@ -54,41 +53,45 @@ passport.deserializeUser(function (user, done) {
   done(null, user);
 });
 
+// app.use(express.static(__dirname + "/public"));
+// app.use(
+//   "/build/",
+//   express.static(path.join(__dirname, "/node_modules/three/build"))
+// );
+// app.use(
+//   "/jsm/",
+//   express.static(path.join(__dirname, "/node_modules/three/examples/jsm"))
+// );
+// app.set("view engine", "ejs");
+// // app.use(express.static(path.resolve(__dirname, "../wastetracker/build")));
 
-app.use(express.static(__dirname + "/public"));
-app.use(
-  "/build/",
-  express.static(path.join(__dirname, "/node_modules/three/build"))
-);
-app.use(
-  "/jsm/",
-  express.static(path.join(__dirname, "/node_modules/three/examples/jsm"))
-);
-app.set("view engine", "ejs");
-// app.use(express.static(path.resolve(__dirname, "../wastetracker/build")));
-
-app.get("/", (req, res) => {
-  res.render("index");
-});
-app.get("/about", (req, res) => {
-  res.render("about");
-});
-app.get("/contact", (req, res) => {
-  res.render("contact");
-});
-app.get("/map", (req, res) => {
-  res.render("home");
-});
+// app.get("/", (req, res) => {
+//   res.render("index");
+// });
+// app.get("/about", (req, res) => {
+//   res.render("about");
+// });
+// app.get("/contact", (req, res) => {
+//   res.render("contact");
+// });
+// app.get("/map", (req, res) => {
+//   res.render("home");
+// });
 // app.get("*", (req, res) => {
 //   res.sendFile(path.resolve(__dirname, "../wastetracker/build", "index.html"));
 // });
-
 
 app.use("/api/pins", pinRoute);
 app.use("/api/users", userRoute);
 app.use("/api/volunteer", volRoute);
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  });
+}
 app.listen(PORT, () => {
   console.log(`Server Running on port ${PORT}`);
 });

0e6d36d22e76ef15a2f3a69f90a61b45f271eef0
Amit Batra <75168062+amitbatra31@users.noreply.github.com>
2021-12-15 20:35:02 +0530

    Fix4

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ app.use("/api/volunteer", volRoute);
 
 const PORT = process.env.PORT || 4000;
 app.use(express.static(path.join(__dirname, "build")));
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 app.listen(PORT, () => {
--- routes/pins.js
+++ routes/pins.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Pin = require("../models/pin");
+const Pin = require("../models/Pin");
 
 //create a pin
 router.post("/", async (req, res) => {

cd65afa105559aeb68f142643829aefa18d02e89
Amit Batra <75168062+amitbatra31@users.noreply.github.com>
2021-12-15 17:36:05 +0530

    change server.js

--- index.js
+++ index.js
@@ -86,12 +86,10 @@ app.use("/api/users", userRoute);
 app.use("/api/volunteer", volRoute);
 
 const PORT = process.env.PORT || 4000;
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
-  });
-}
+app.use(express.static(path.join(__dirname, "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 app.listen(PORT, () => {
   console.log(`Server Running on port ${PORT}`);
 });
